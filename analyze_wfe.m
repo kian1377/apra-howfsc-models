fdab875e35616ee5d66dec7d271d0f56baef2a7f
Kian Milani <kianmilani@arizona.edu>
2023-03-07 17:23:36 -0700

    misaligning optics in 6DOF and updated prop model

--- analyze_wfe.m
+++ analyze_wfe.m
@@ -32,8 +32,18 @@ m2_m3 = theLDE.GetSurfaceAt(4).Thickness + theLDE.GetSurfaceAt(5).Thickness;
 m3_fsm = theLDE.GetSurfaceAt(6).Thickness;
 fsm_fp = theLDE.GetSurfaceAt(10).Thickness;
 
-misalignment = 
-theLDE.GetSurfaceAt(3).Thickness = m1_m2 + 1e-3;
+keys = ["decenter_x" "decenter_y" "decenter_z" "tilt_x" "tilt_y" "tilt_z"];
+m1_vals = [0 1e-6 1e-6 0.0001 0 0];
+m1_vals(1:3) = m1_vals(1:3)*1e3;
+m1_misalignment = dictionary(keys, m1_vals);
+
+% misalign the primary according to the specified values
+theLDE.GetSurfaceAt(3).Thickness = m1_m2 + m1_misalignment("decenter_z");
+theLDE.GetSurfaceAt(3).TiltDecenterData.BeforeSurfaceDecenterX = m1_misalignment("decenter_x");
+theLDE.GetSurfaceAt(3).TiltDecenterData.BeforeSurfaceDecenterY = m1_misalignment("decenter_y");
+theLDE.GetSurfaceAt(3).TiltDecenterData.BeforeSurfaceTiltX = m1_misalignment("tilt_x");
+theLDE.GetSurfaceAt(3).TiltDecenterData.BeforeSurfaceTiltY = m1_misalignment("tilt_y");
+theLDE.GetSurfaceAt(3).TiltDecenterData.BeforeSurfaceTiltZ = m1_misalignment("tilt_z");
 
 % sysField = theSystem.SystemData.Fields;
 
@@ -52,16 +62,18 @@ WFMap.ApplyAndWaitForCompletion();
 % get results
 WFMapResults = WFMap.GetResults();
 wfe_data = flipud(WFMapResults.DataGrids(1).Values.double);
+wfe_data(isnan(wfe_data)) = 0;
 
 % result is in waves so we convert to nm
 wfe_data = wfe_data * wave;
 imagesc(wfe_data)
+colorbar('eastoutside', 'FontSize',12)
+axis image
+% imshow(wfe_data, [])
 
-theSystem.Close(false);
-theApplication.CloseApplication();
+CloseZemax(theApplication)
 
 % save the WFE data to a fits file
-wfe_data(isnan(wfe_data)) = 0;
 fname = 'wfe_on_axis.fits';
 fitswrite(wfe_data, fname, 'WriteMode','overwrite')
 

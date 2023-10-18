96a9f6d6c7192c11fb132afc8e073bd0ae9b7207
Kian Milani <kianmilani@email.arizona.edu>
2023-10-18 11:35:37 -0700

    reorganizing repo and making things cleaner

--- CloseZemax.m
+++
@@ -1,9 +0,0 @@
-function CloseZemax(TheApplication)
-
-% theSystem.Close(false);
-% TheApplication.CloseApplication();
-
-TheApplication.PrimarySystem.Close(false);
-TheApplication.CloseApplication();
-LogMessage(strcat('Closed Zemax OpticStudio connection.'))
-end
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # apra-howfsc-models
 Repo for the telescope models to test HOWFSC algorithms upon. 
+
+
+
+
+

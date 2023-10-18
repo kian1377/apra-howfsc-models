0d578954157775dc3e166c9a993df0d78b6d55b3
Kian Milani <kianmilani@email.arizona.edu>
2023-10-18 14:37:00 -0700

    major updates reorganizing the repo and running EFC with PWP

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .import math_module, imshows, utils
-from .pop_models import fresnel_coro_poppy, fraunhofer_coro_poppy
-from .pop_models import fraunhofer_coro, agpm, dm
-
-__version__ = '0.1.0'
-
---
+++ apra_pop_models/__init__.py
@@ -0,0 +1,5 @@
+from .import math_module, imshows, utils
+from . import fresnel_coro_poppy, fraunhofer_coro_poppy, fraunhofer_coro, agpm, dm
+
+__version__ = '0.1.0'
+

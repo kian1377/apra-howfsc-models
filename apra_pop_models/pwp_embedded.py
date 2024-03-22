707c772ae24dc2afc6cb7f470671d68392352abb
kian1377 <kianmilani@arizona.edu>
2024-03-22 12:19:02 -0400

    testing on the p5040 from Homer

--- apra_pop_models/pwp_embedded.py
+++ apra_pop_models/pwp_embedded.py
@@ -12,14 +12,16 @@ import time
 import copy
 
 import sys
-sys.path.insert(len(sys.path), 'home/apra/Projects/P5040/P5040_test_software/EFC/python_host')
+# sys.path.insert(len(sys.path), 'home/apra/Projects/P5040/P5040_test_software/EFC/python_host')
+sys.path.insert(len(sys.path), '/home/apra/Projects/P5040/P5040_test_software/EFC/python_host')
 import efc_host_utils
 
+
 def run_pwp_bp(sysi, 
+               embedded_controller,
                control_mask, 
                probes,
                jacobian,
-               embedded_controller,
                plot=False,
                plot_est=False):
     """ 
@@ -58,15 +60,19 @@ def run_pwp_bp(sysi,
 
     for i,probe in enumerate(probes):
         for amp in amps:
-            sysi.add_dm(amp*probe)
-            psf = sysi.snap()
+            if callable(getattr(sysi, "add_dm", None)):
+                sysi.add_dm(amp*probe)
+                psf = sysi.snap()
+                sysi.add_dm(-amp*probe) # remove probe from DM
+            elif callable(getattr(sysi, "add_dm1", None)):
+                sysi.add_dm1(amp*probe)
+                psf = sysi.snap()
+                sysi.add_dm1(-amp*probe) # remove probe from DM
                 
             if amp==-1: 
                 In.append(psf)
             else: 
                 Ip.append(psf)
-                
-            sysi.add_dm(-amp*probe) # remove probe from DM
             
         if plot:
             imshows.imshow3(Ip[i], In[i], Ip[i]-In[i], lognorm1=True, lognorm2=True, pxscl=sysi.psf_pixelscale_lamD)
@@ -75,7 +81,9 @@ def run_pwp_bp(sysi,
     I_diff = xp.zeros((probes.shape[0], Nmask))
     for i in range(len(probes)):
         probe = xp.array(probes[i])
-        E_probe = embedded_controller.do_PWP(probe[sysi.dm_mask.astype(bool)])
+        probe_command = xp.zeros(jacobian.shape[1])
+        probe_command[:sysi.Nacts] = probe[sysi.dm_mask.astype(bool)]
+        E_probe = embedded_controller.compute_probe_field(probe_command)
         E_probe = E_probe[::2] + 1j*E_probe[1::2]
             
         if plot:
